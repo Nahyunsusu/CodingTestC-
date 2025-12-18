27992e73a4c4429ffcc754aedbb10111db0796d5
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2025-12-18 17:35:17 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.27 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution
+{
+    public int[] solution(int[] num_list, int n)
+    {
+        int[] answer = new int[num_list.Length-(n - 1)];
+
+        Array.Copy(num_list, n - 1, answer, 0,  answer.Length);
+
+        return answer;
+    }
+}
