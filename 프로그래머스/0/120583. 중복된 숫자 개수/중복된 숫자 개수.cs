cd245b2b8e97596cb2fa35d5c9cc3a70117311a9
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2025-12-19 15:14:09 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.27 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] array, int n)
+    {
+        int answer = 0;
+
+        foreach(int a in array)
+        {
+            if (a == n)
+                answer++;
+        }
+
+        return answer;
+    }
+}
