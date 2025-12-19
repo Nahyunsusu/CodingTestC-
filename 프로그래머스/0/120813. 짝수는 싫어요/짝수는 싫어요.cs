c8278ed9c3f1f56b2c8a58f6d19b1c0ba80f061f
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2025-12-19 15:25:04 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.19 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution
+{
+    public int[] solution(int n)
+    {
+        int num = 0;
+
+        if (n % 2 == 0)
+            num = n / 2;
+        else
+            num = n / 2 + 1;
+
+        int[] answer = new int[num];
+
+        for (int i = 0; i < num; i++)
+        {
+            answer[i] = 2 * i + 1;
+        }
+
+
+        return answer;
+    }
+}
