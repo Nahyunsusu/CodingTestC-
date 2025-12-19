261efed69700722d69b018f5fca1a50836507244
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2025-12-19 15:01:13 +0900

    [level 0] Title: 카운트 업, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Solution
+{
+    public int[] solution(int start_num, int end_num)
+    {
+        int[] answer = new int[end_num - start_num+1];
+
+        /*
+        for(int i=start_num;i<end_num;i++)
+        {
+            answer[i] = i;
+        }
+        */
+
+        int Count = 0;
+
+        for(int i=start_num;i<=end_num;i++)
+        {
+            answer[Count] = i;
+            Count++;
+        }
+
+        return answer;
+    }
+}
