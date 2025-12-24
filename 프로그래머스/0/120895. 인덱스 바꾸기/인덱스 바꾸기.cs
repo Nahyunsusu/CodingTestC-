b913600325959898987ffcc4c280d581c14ca71a
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2025-12-24 17:17:14 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.35 ms, Memory: 30.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution
+{
+    public string solution(string my_string, int num1, int num2)
+    {
+        char[] chars = my_string.ToCharArray();
+
+        char temp = chars[num1];
+        chars[num1] = chars[num2];
+        chars[num2] = temp;
+
+        return new string(chars);
+    }
+}
