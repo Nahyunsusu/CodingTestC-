5b90ec8126b3551e9a160d9f139048aa6b574bc6
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2025-12-29 13:04:05 +0900

    [level 1] Title: 평균 구하기, Time: 0.23 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cs
@@ -0,0 +1,14 @@
+public class Solution
+{
+    public double solution(int[] arr)
+    {
+        double answer = 0;
+
+        foreach(int i in arr)
+        {
+            answer += i;
+        }
+
+        return answer/arr.Length;
+    }
+}
