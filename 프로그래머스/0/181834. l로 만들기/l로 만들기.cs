a79a3044113c02dc4c68ab13ef0c130fa18282d2
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2025-12-29 14:16:48 +0900

    [level 0] Title: l로 만들기, Time: 5100.55 ms, Memory: 47.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public string solution(string myString)
+    {
+        string answer = "";
+
+
+        for(int i=0;i<myString.Length;i++)
+        {
+            if (myString[i] < 'l')
+            {
+                answer += 'l';
+            }
+            else
+            {
+                answer += myString[i];
+            }
+        }
+
+        return answer;
+    }
+}
