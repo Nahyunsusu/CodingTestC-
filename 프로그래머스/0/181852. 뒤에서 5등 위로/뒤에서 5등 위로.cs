f084d01ef753fa63506bbadaaa56e0928159664f
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-01-02 09:42:05 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 2.47 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(int[] num_list)
+    {
+        List<int> temp = new List<int>();
+
+        Array.Sort(num_list);
+
+        for(int i=5;i<num_list.Length;i++)
+        {
+            temp.Add(num_list[i]);
+        }
+
+        return temp.ToArray();
+    }
+}
