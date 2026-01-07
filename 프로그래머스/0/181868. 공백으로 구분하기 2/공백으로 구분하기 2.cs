bc20b8cee11261f28820c51668699c55595e8ac0
4awol <skgustn3355@gmail.com>
2026-01-07 09:49:43 +0900

    올려잇

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cs
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cs
@@ -1,22 +1,9 @@
 using System;
-using System.Collections.Generic;
 
 public class Solution
 {
     public string[] solution(string my_string)
     {
-        string[] words = my_string.Split(' ');
-
-        List<string> result = new List<string>();
-
-        foreach (string s in words)
-        {
-            if (s != "")
-            {
-                result.Add(s);
-            }
-        }
-
-        return result.ToArray();
+        return my_string.Split(' ', StringSplitOptions.RemoveEmptyEntries);
     }
 }
