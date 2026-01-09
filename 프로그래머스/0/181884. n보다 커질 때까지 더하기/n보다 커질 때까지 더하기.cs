d156ef5aa9305dbd82e4ef3c55a813aaed9b3167
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-01-09 09:38:35 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.18 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers, int n) 
+    {
+        int answer = 0;
+        
+        for(int i=0;i<numbers.Length;i++)
+        {
+            answer += numbers[i];
+            if(answer > n)
+                return answer;
+        }
+        
+        return answer;
+    }
+}
