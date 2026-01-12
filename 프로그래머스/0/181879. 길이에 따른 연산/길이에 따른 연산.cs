2d02d3489b7567555494ca86efd9c93d1358cac1
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-01-12 17:18:56 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cs
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cs
@@ -1,27 +1,8 @@
 using System;
 
-public class Solution
-{
-    public int solution(int[] num_list)
-    {
+public class Solution {
+    public int solution(int[] num_list) {
         int answer = 0;
-
-        if (num_list.Length > 10)
-        {
-            foreach (int i in num_list)
-            {
-                answer = answer + i;
-            }
-
-            return answer;
-        }
-
-        answer = 1;
-
-        foreach(int i in num_list)
-        {
-            answer =answer* i;
-        }
         return answer;
     }
 }
