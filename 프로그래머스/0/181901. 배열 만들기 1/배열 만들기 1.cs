0165d567f9c9f0fa5764181097e83537fdcc596d
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-01-12 19:44:19 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.78 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(int n, int k)
+    {
+        List<int> answer = new List<int>();
+
+        for(int i = k;i<=n;i+=k)
+        {
+            answer.Add(i);
+        }
+
+        return answer.ToArray();
+    }
+}
