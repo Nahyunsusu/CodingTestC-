f713bd8719991bf7f742a4f5ff9044ee1be9f0c2
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-01-20 10:09:43 +0900

    [level 0] Title: 주사위의 개수, Time: 0.26 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cs
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cs
@@ -1,14 +1,16 @@
 using System;
 
-public class Solution {
-    public int solution(int[] box, int n) {
-        int answer = 0;
-        
-        int width = box[0] /n;
-        int height = box[1] /n;
-        int depth = box[2] / n;
-        
-        
-        return width * height * depth;
+public class Solution
+{
+    public int solution(int[] box, int n)
+    {
+        int answer = 1;
+
+        for(int i=0;i<box.Length;i++)
+        {
+            answer *= box[i] / n;
+        }
+
+        return answer;
     }
 }
