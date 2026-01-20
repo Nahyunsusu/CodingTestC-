24946313483945ab89c91e4b710ec14553363aa6
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-01-20 17:02:33 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.97 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class Solution
+{
+    public string solution(string s)
+    {
+        string answer = "";
+
+        string[] temp = s.Split(' ');
+
+        for (int i = 0; i < temp.Length; i++)
+        {
+            for (int j = 0; j < temp[i].Length; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    answer += char.ToUpper(temp[i][j]);
+                }
+                else
+                {
+                    answer += char.ToLower(temp[i][j]);
+                }
+            }
+
+            if (i < temp.Length - 1)
+            {
+                answer += " ";
+            }
+        }
+
+        return answer;
+    }
+}
