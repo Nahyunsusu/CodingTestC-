fc835b2c8f32e94081a4c057111f2e131c147a8f
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-02-11 17:53:19 +0900

    [level 0] Title: 1로 만들기, Time: 0.31 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] num_list)
+    {
+        int answer = 0;
+        int count = 0;
+
+        for(int i=0;i<num_list.Length;i++)
+        {
+            while (num_list[i] != 1)
+            {
+                if (num_list[i] % 2 == 0)
+                {
+                    num_list[i] /= 2;
+                }
+                else
+                {
+                    num_list[i] = (num_list[i] - 1) / 2;
+                }
+
+                count++;
+            }
+
+            answer += count;
+            count = 0;
+        }
+
+        return answer;
+    }
+}
