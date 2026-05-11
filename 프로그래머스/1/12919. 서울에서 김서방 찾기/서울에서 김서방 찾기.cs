37e439d5207183e32a9615c6063487cf035042e6
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-05-11 14:20:39 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.37 ms, Memory: 30.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Solution
+{
+    public string solution(string[] seoul)
+    {
+        string answer = "";
+
+        for (int i = 0; i < seoul.Length; i++)
+        {
+            if (seoul[i] == "Kim")
+            {
+                answer = $"김서방은 {i}에 있다";
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
