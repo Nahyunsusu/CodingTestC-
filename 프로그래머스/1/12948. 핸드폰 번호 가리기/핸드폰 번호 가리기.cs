37fc1260e84b2d61b9eaf325f96a8be72a06992a
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-05-11 16:21:47 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.27 ms, Memory: 30.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -0,0 +1,22 @@
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Solution
+    {
+        public string solution(string phone_number)
+        {
+            string answer = "";
+
+            for(int i=0;i<phone_number.Length -4;i++)
+            {
+                answer += '*';
+            }
+
+            for(int i=phone_number.Length- 4;i<phone_number.Length;i++)
+            {
+                answer += phone_number[i];
+            }
+
+            return answer;
+        }
+    }
